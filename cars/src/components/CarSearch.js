ddca5c190159ea5078422ed14893b99180e0f384
hi6724 <hi6724@gmail.com>
2022-12-11 22:31:26 +0900

    carSearch

--- cars/src/components/CarSearch.js
+++ cars/src/components/CarSearch.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeSearchTerm } from "../store";
 
 function CarSearch() {
-  return <div>CarSearch</div>;
+  const dispatch = useDispatch();
+  const searchTerm = useSelector((state) => state.cars.searchTerm);
+  const handleSearchTermChange = (e) => {
+    dispatch(changeSearchTerm(e.target.value));
+  };
+  return (
+    <div className="list-header">
+      <h3 className="title is-3">My Cars</h3>
+      <div className="search field is-horizontal">
+        <label className="label">Search</label>
+        <input
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+          className="input"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default CarSearch;
