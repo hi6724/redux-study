4aa4acf3e8809c9ef3e149af84cb403cb0894faa
hi6724 <hi6724@gmail.com>
2022-12-11 22:01:20 +0900

    carList

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function CarList() {
-  return <div>CarList</div>;
+  const cars = useSelector((state) => state.cars.data);
+  const handleCarDelete = (car) => {};
+
+  const renderedCars = cars.map((car) => (
+    <div key={car.id} className="panel">
+      <p>
+        {car.name} - ${car.cost}
+      </p>
+      <button onClick={() => handleCarDelete(car)} className="button is-danger">
+        Delete
+      </button>
+    </div>
+  ));
+
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
